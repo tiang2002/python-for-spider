19939fd69b05fa7c2c7f94c398b4063c4e10303d
tiang2002 <74613129+tiang2002@users.noreply.github.com>
2022-03-02 12:25:03 +0800

    Update seat.py

--- seat.py
+++ seat.py
@@ -128,6 +128,7 @@ print('请严格按照括号内的格式输入时间！')
 start_time = input('请输入起始时间（12-30）')
 end_time = input('请输入结束时间（12-30）')
 date = input('请输入预约日期（1.今天，2.明天）')
+date_flag = date
 building = input('请选择分馆（1.信息分馆）')
 rooms = input('请选择房间\n1.二楼到四楼预约\n2.自主学习区\n3.一楼苹果区预约\n')
 if rooms == '1':
@@ -183,7 +184,7 @@ elif date == '2':
     now = datetime.now()
     now_time = now.strftime('%m-%d')
     now_hour = datetime.strptime(str(now.hour) + '-' + str(now.minute), '%H-%M')
-    target = datetime.strptime(str(now.year)+'-22-45', '%Y-%H-%M')
+    target = datetime.strptime(str(now.year) + '-22-45', '%Y-%H-%M')
     less = target - now_hour
     while now_time < '22-45':
         now = datetime.now()
@@ -205,12 +206,13 @@ url = 'https://seat.lib.whu.edu.cn/'
 browser.get(url)
 
 browser.find_element_by_xpath(r'/html/body/div[4]/div[2]/div[2]/dl/input').click()
-browser.find_element_by_xpath(r'/html/body/div/div[2]/div[3]/div[2]/div[4]/div[2]/form/div[1]/input').send_keys(
+browser.find_element_by_xpath(r'//*[@id="username"]').send_keys(
     user)
-browser.find_element_by_xpath(r'/html/body/div/div[2]/div[3]/div[2]/div[4]/div[2]/form/div[2]/input[1]').send_keys(
+browser.find_element_by_xpath(r'//*[@id="password"]').send_keys(
     password)
 browser.find_element_by_xpath(r'//*[@id="casLoginForm"]/p[2]/button').click()
-time.sleep(2)
+while browser.current_url != 'https://seat.lib.whu.edu.cn/':
+    time.sleep(1)
 cookies = browser.get_cookies()
 cookies = cookies[0]['name'] + '=' + cookies[0]['value'] + '; ' + cookies[1]['name'] + '=' + cookies[1]['value']
 print(cookies)
@@ -228,7 +230,14 @@ while 1:
 
         # pprint(res)
         seat_id = res['seatStr']
-        seat_id = re.findall('id="seat_(.*?)"', seat_id)
+        seat_free = re.findall('<li class="free"(.*?)li>', seatStr)
+        seat_id = []
+        for each in seat_free:
+            if "新增" in each:
+                pass
+            else:
+                temp = re.findall('id="seat_(.*?)"', each)[0]
+                seat_id.append(temp)
         if len(seat_id) > 0:
             flag = 1
             break
@@ -245,16 +254,26 @@ token = browser.find_element_by_xpath('//*[@id="SYNCHRONIZER_TOKEN"]').get_attri
 print(token)
 reserve_url = 'https://seat.lib.whu.edu.cn/selfRes'
 
-payload = {
-    'SYNCHRONIZER_TOKEN': token,
-    'SYNCHRONIZER_URI': '/self',
-    'date': '',
-    'seat': str(seat_id),
-    'start': str(start_time),
-    'end': str(end_time),
-    'authid': '-1'
-}
-
+if date_flag != '2':
+    payload = {
+        'SYNCHRONIZER_TOKEN': token,
+        'SYNCHRONIZER_URI': '/self',
+        'date': '',
+        'seat': str(seat_id),
+        'start': str(start_time),
+        'end': str(end_time),
+        'authid': '-1'
+    }
+else:
+    payload = {
+        'SYNCHRONIZER_TOKEN': token,
+        'SYNCHRONIZER_URI': '/self',
+        'date': date,
+        'seat': str(seat_id),
+        'start': str(start_time),
+        'end': str(end_time),
+        'authid': '-1'
+    }
 reservation = get_res_post(reserve_url, cookies, payload)
 print(reservation)
 print(reservation.text)
